47a2cd7cb41f548413dc41537872bd147c62326a
TabibWasit <tabib.wasit@live.vu.edu.au>
2023-09-22 20:55:21 +1000

    User page and Interactice chatbot ready! Finishing touches required!

---
+++ app/src/main/java/com/example/sage/API.java
@@ -0,0 +1,8 @@
+package com.example.sage;
+
+public class API {
+
+    public static String API_URL = "https://api.openai.com/v1/completions";
+
+    public static String API = "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";
+}
